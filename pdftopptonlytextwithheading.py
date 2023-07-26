73f6b1f7897675da93f032a80c518a5880d97e93
02kartikey <131267699+02kartikey@users.noreply.github.com>
2023-07-27 01:27:21 +0530

    Update pdftopptonlytextwithheading.py

--- pdftopptonlytextwithheading.py
+++ pdftopptonlytextwithheading.py
@@ -2,33 +2,6 @@ from pptx import Presentation
 from pptx.util import Inches
 from PyPDF2 import PdfReader
 import openai
-import fitz  # PyMuPDF library
-import os
-
-# Function to extract images from a specific page of the PDF
-def extract_images_from_pdf(pdf_file, page_number):
-    images = []
-    pdf_document = fitz.open(pdf_file)
-    page = pdf_document[page_number - 1]
-    image_list = page.get_images(full=True)
-
-    # Get the full path of the "images" directory based on the current working directory
-    current_directory = os.getcwd()
-    images_directory = os.path.join(current_directory, "images")
-
-    # Create the "images" directory if it doesn't exist
-    if not os.path.exists(images_directory):
-        os.makedirs(images_directory)
-
-    for img_index, img in enumerate(image_list):
-        image = pdf_document.extract_image(img[0])
-        image_name = f"image_page{page_number}_{img_index + 1}.png"
-        image_path = os.path.join(images_directory, image_name)  # Save images in the "images" subdirectory
-        with open(image_path, "wb") as img_file:
-            img_file.write(image["image"])
-        images.append(image_path)
-
-    return images
 
 # Function to extract text from a specific page of the PDF
 def extract_text_from_pdf(pdf_file, page_number):
@@ -41,7 +14,7 @@ def extract_text_from_pdf(pdf_file, page_number):
 # Function to summarize the text using OpenAI API
 def summarize_with_openai(text):
     # Replace with your actual OpenAI API key
-    openai.api_key = "OpenAI-Key"
+    openai.api_key = "OpenAPI KEY"
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=text,
@@ -51,39 +24,19 @@ def summarize_with_openai(text):
     summary = response['choices'][0]['text']
     return summary
 
-# Function to extract images from a specific page of the PDF
-def extract_images_from_pdf(pdf_file, page_number):
-    images = []
-    pdf_document = fitz.open(pdf_file)
-    page = pdf_document[page_number - 1]
-    image_list = page.get_images(full=True)
-
-    for img_index, img in enumerate(image_list):
-        image = pdf_document.extract_image(img[0])
-        image_name = f"image_page{page_number}_{img_index + 1}.png"
-        image_path = f"images/{image_name}"  # Save images in a subdirectory named "images"
-        with open(image_path, "wb") as img_file:
-            img_file.write(image["image"])
-        images.append(image_path)
-
-    return images
-
 def main():
-    pdf_file = '####.pdf'  # Replace with the path to your PDF file
+    pdf_file = '#######.pdf'  # Replace with the path to your PDF file
     total_pages = 0
     summaries = []
-    all_images = []
 
     with open(pdf_file, 'rb') as file:
         reader = PdfReader(file)
         total_pages = len(reader.pages)
 
-    print(f"Total Pages in the PDF: {total_pages}\n")
+        # Extract the title from the first page of the PDF
+        title_text = extract_text_from_pdf(pdf_file, 1)
 
-    # Extract all images once before the loop
-    for page_number in range(1, total_pages + 1):
-        images = extract_images_from_pdf(pdf_file, page_number)
-        all_images.append(images)
+    print(f"Total Pages in the PDF: {total_pages}\n")
 
     for page_number in range(1, total_pages + 1):
         # Step 1: Extract the text from the PDF page
@@ -95,9 +48,6 @@ def main():
         # Append the summary to the list
         summaries.append(summary)
 
-        # Step 3: Extracted images from the PDF page (already done before the loop)
-        images = all_images[page_number - 1]
-
         # Display the summarized output for each page (optional)
         print(f"Summarized Output for Page {page_number}:")
         print(summary)
@@ -105,31 +55,20 @@ def main():
 
     # Create a new presentation
     prs = Presentation()
-    slide_layout = prs.slide_layouts[1]  # Choose a slide layout (Title and Content)
+    slide_layout = prs.slide_layouts[1]  # Choose a slide layout (Title Slide with Content)
 
     # Add a slide for each summary
-    for page_number, (summary, images) in enumerate(zip(summaries, all_images), start=1):
+    for page_number, summary in enumerate(summaries, start=1):
         slide = prs.slides.add_slide(slide_layout)
-
-        # Set the title text
         title = slide.shapes.title
-        title.text = f"Here is your {page_number} Summary"
-
-        # Set the content text only if the layout has a content placeholder (index 1)
+        title.text = f"{title_text} - Page {page_number} Summary"
         content = slide.placeholders[1]
-        if content.placeholder_format.idx == 1:
-            content.text = summary
-
-        # Add the images to the slide
-        for img_path in images:
-            slide.shapes.add_picture(img_path, Inches(2), Inches(2), height=Inches(4.5))
+        content.text = summary
 
     # Save the presentation to a PPT file
-    prs.save("summary_presentation_with_image.pptx")
+    prs.save("summary_presentation.pptx")
 
-    print("Presentation with text summaries and images saved successfully!")
-
+    print("Presentation with text summaries saved successfully!")
 
 if __name__ == "__main__":
     main()
-
